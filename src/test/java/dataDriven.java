0b2590668dbdc8c67a132e984163107ba90d86e2
omar <omar_2124@hotmail.com>
2019-04-16 16:48:56 -0500

    Adding excel testase

---
+++ src/test/java/dataDriven.java
@@ -0,0 +1,48 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class dataDriven {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		//FileInputStream arguement
+		FileInputStream fis = new FileInputStream("C://Selenium//ExcelDataDriven//demoData.xlsx"); 
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		
+		int sheets = workbook.getNumberOfSheets();
+		for (int i=0; i<sheets; i++)
+			{
+				if (workbook.getSheetName(i).equalsIgnoreCase("testdata"))
+				{
+					XSSFSheet sheet=workbook.getSheetAt(i);
+					
+					Iterator<Row> rows = sheet.iterator();//Sheet is collection of rows
+					Row firstrow = rows.next();
+					Iterator<Cell> ce = firstrow.cellIterator();//Row is collection of cells
+					int k=0;
+					int column = 0; 
+					while(ce.hasNext())
+					{
+						Cell value = ce.next();
+						if (value.getStringCellValue().equalsIgnoreCase("TestCases"))
+						{
+							column=k;
+						}
+					k++;
+					}
+					System.out.println(column);
+				}
+			}
+		
+		
+	}
+
+}
